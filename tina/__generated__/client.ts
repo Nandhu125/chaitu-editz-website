c3c2397d39ee235f67c08732dc1b62319d908db7
Nandhu125 <dharnanandhu968@gmail.com>
2026-02-19 04:50:39 +0000

    added base new page

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'local-dev-token', queries,  });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: '46cf44010b39953719c5fb630856d4cb25c969f1', queries,  });
 export default client;
   
